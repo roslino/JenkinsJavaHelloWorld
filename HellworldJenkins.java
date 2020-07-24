3178ab4ac6d9979d1398cfdc6c264e86c28613b2
Guy-Ross <guyrossaf@yahoo.fr>
2020-07-24 15:42:37 -0400

    Second java file commit

---
+++ HellworldJenkins.java
@@ -0,0 +1,8 @@
+
+public class HellworldJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("BONJOUR JENKINS - un simple  projet build test via Jenkins");
+    }
+
+}
